ddee6db89611e8ee539a5812ea709e29c6dab75f
Alexandr Voronkov <alebymars@ya.ru>
2024-03-13 01:59:16 +0700

    update (read description)
    package - added redux-persist
    index - added data saving (now the cart should not be reset when the page is reloaded)
    store - added data saving

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,34 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./slices/rootSlice";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+// persisting our rootReducer
+const persistedRootReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: {
-    root: rootReducer,
+    root: persistedRootReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }),
 });
 
